a16bc69b78dcee842ef035f31d3b3562ab054fdc
happiguru <hguruman@gmail.com>
2021-07-28 10:41:18 +0400

    Add react Router to the project

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ name, handleClick }) {
-  return (
-    <>
-      <button
-        type="button"
-        onClick={() => { handleClick(name); }}
-      >
-        {name}
-      </button>
-    </>
-  );
-}
+const Button = ({ name, handleClick }) => (
+
+  <button
+    type="button"
+    onClick={() => { handleClick(name); }}
+  >
+    {name}
+  </button>
+);
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Quote from './pages/quotes';
 import App from './components/App';
+import Header from './components/Header';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={App} />
+        <Route exact path="/quotes" component={Quote} />
+      </Switch>
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root'),
 );
